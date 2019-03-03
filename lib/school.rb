72a9c613b61f86602010fecbfa059b8dd6d4748d
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-03 20:14:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+#writing a domain model that represents a School
+#part 1: creat a class, school in the lib directory that can be initialized with a name
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+def add_student(student, grade)
+  if @roster[grade].nil? || @roster[grade].empty? 
+    @roster[grade] << student
+  else
+    @roster[grade] = [student]
+  end
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each do |grade, student|
+    student.sort!
+  end
+end
+
+end
